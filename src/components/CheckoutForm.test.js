fc405c85d9456e711509ed90a8a81eda78df8de3
wb1110 <strengthcoachwb@gmail.com>
2021-12-03 08:56:19 -0600

    testing almost done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
 import { render, waitFor, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -10,5 +11,25 @@ test("renders without errors", () => {
 });
 
 test("shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+
+    const firstName = screen.getByLabelText(/First Name/i);
+    const lastName = screen.getByLabelText(/Last Name/i);
+    const address = screen.getByLabelText(/Address/i);
+    const city = screen.getByLabelText(/City/i);
+    const state = screen.getByLabelText(/State/i);
+    const zip = screen.getByLabelText(/Zip/i);
+    const submit = screen.getByRole('button', {name: /checkout/i});
+
+    userEvent.type(firstName, 'William')
+    userEvent.type(lastName, 'Buchanan')
+    userEvent.type(address, '5555 Address')
+    userEvent.type(city, 'Dallas')
+    userEvent.type(state, 'TX')
+    userEvent.type(zip, '75555')
+    userEvent.click(submit)
+
+    expect(firstName).toHaveValue('William')
+
     
 });
